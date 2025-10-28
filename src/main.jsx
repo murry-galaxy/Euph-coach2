2edd7f5b8139f94f1851d14ff684c1fa233fa552
murry-galaxy <david.murat@gmail.com>
2025-10-28 11:27:45 +0000

    added // Register service worker for PWA support if

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+// Register service worker for PWA support
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").catch(console.error);
+  });
+}
